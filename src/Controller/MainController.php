8f3d567af4ce9819bd67b61a2d540301808afc41
bernard-ng <ngandubernard@gmail.com>
2024-02-25 17:51:48 +0200

    7 twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Repository\PostRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,13 +14,19 @@ class MainController extends AbstractController
     #[Route('', name: 'index')]
     public function index(PostRepository $repository): Response
     {
-
         $posts = $repository->findAll();
 
-        dd($posts);
-
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'posts' => $posts
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('main/show.html.twig', [
+            'post' => $post
         ]);
     }
 }
+
